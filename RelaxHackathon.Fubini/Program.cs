0f06572c22137664d3e74246991f0bf589a9737d
Max Brauer <ma.brauer@live.de>
2021-03-13 10:07:07 +0100

    init project

---
+++ RelaxHackathon.Fubini/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RelaxHackathon.Fubini
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
